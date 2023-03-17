c3b321e63f08110f17d6d92d06b5450296296306
Parakh <parakhpawar623@gmail.com>
2023-03-18 01:47:52 +0530

    all files

---
+++ solo/lib/main.dart
@@ -0,0 +1,23 @@
+
+import 'package:flutter/material.dart';
+import 'firstpage.dart';
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: FirstPage(),
+    );
+  }
+}
